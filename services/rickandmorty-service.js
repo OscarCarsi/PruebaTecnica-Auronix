e2e5c571319502e078c89d0967a28d3d4fa4ceb4
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-12-11 16:54:19 -0600

    add array to save the 826 characters, count the pages and save de total pages

--- services/rickandmorty-service.js
+++ services/rickandmorty-service.js
@@ -1,5 +1,8 @@
 const URL_ENDPOINT = "https://rickandmortyapi.com/api/character/";
 const getCaracteres = async () => {
+    let allCharacters = [];
+    let page = 1;
+    let totalPages = 1;
     try{
         const response = await fetch(URL_ENDPOINT, {
             method: "GET",
