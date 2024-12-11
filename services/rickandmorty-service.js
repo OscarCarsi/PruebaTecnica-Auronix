9eb8cfd388429de62642257fe26f14937705c4d1
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-12-11 16:43:19 -0600

    method to can get the characters using fetch with async and await

--- services/rickandmorty-service.js
+++ services/rickandmorty-service.js
@@ -1,1 +1,13 @@
 const URL_ENDPOINT = "https://rickandmortyapi.com/api/character/";
+const getCaracteres = async () => {
+    try{
+        const response = await fetch(URL_ENDPOINT, {
+            method: "GET",
+        });
+        const data = await response.json();
+        console.log (data);
+    }
+    catch(error){
+        console.error("Error to get characters", error);
+    }
+}
