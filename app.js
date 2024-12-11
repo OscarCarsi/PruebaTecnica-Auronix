d89f1d7afa941aec1a131a5e538844e50ed0129f
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-12-11 17:42:03 -0600

    call the functions into main, get the characters, filter the alive characters and modify name, finally create a json with the results

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import { getCaracteres } from "./services/rickandmorty-service.js";
 import { filterAliveResults } from "./utils/filter.js";
 import { remplaceName } from "./utils/modifyname.js";
+
+const main = async () => {
+    const characters = await getCaracteres();
+    const charactersAlive = filterAliveResults(characters);
+    const charactersWithNameModified = remplaceName(charactersAlive);
+    const resultJson ={
+        results: charactersWithNameModified
+    }
+    console.log(resultJson);
+}
+
+main();
