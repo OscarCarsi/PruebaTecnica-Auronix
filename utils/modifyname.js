a6ce95bcd829c0912b8046a5b7a8b95bd2360c18
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-12-11 17:26:50 -0600

    return only the requested props and replace blank spaces with underscores

--- utils/modifyname.js
+++ utils/modifyname.js
@@ -1,4 +1,10 @@
 const remplaceName = (results) => {
     return results.map((result) => {
+        return{
+            id: result.id,
+            name: result.name.replace(" ", "_"),
+            status: result.status,
+            gender: result.gender
+        }
     });
 }
