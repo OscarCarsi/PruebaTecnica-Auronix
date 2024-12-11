cd4191db3e40754502225c4219c1dc22feef9bdc
OscarCarsi <oscarolivarezcarsi@gmail.com>
2024-12-11 16:55:36 -0600

    Create a while loop to get all pages to the api, also save the characters into array, and return all characters

--- services/rickandmorty-service.js
+++ services/rickandmorty-service.js
@@ -1,14 +1,20 @@
 const URL_ENDPOINT = "https://rickandmortyapi.com/api/character/";
+
 const getCaracteres = async () => {
     let allCharacters = [];
     let page = 1;
     let totalPages = 1;
     try{
-        const response = await fetch(URL_ENDPOINT, {
-            method: "GET",
-        });
-        const data = await response.json();
-        console.log (data);
+        while(page <= totalPages){
+            const response = await fetch(`${URL_ENDPOINT}?page=${page}`, {
+                method: "GET",
+            });
+            const data = await response.json();
+            allCharacters = allCharacters.concat(data.results);
+            totalPages = data.info.pages;
+            page++;
+        }
+        return allCharacters;
     }
     catch(error){
         console.error("Error to get characters", error);
